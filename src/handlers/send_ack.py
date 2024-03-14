caba0b17b734a1f53947b311c3f95f1f81539600
kroskinskiis <ivo.kroskinski@tno.nl>
2024-03-14 11:45:19 +0100

    Remove deprecated files

--- src/handlers/send_ack.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.client as mqtt
-
-from messageFactory import generateAckMessage
-
-
-def send_ack(mqttc: mqtt.Client, message_id: str):
-    ack = generateAckMessage(message_id)
-
-    mqttc.publish("soarca", payload=ack.model_dump_json(), qos=1)
--- src/handlers/send_nack.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.client as mqtt
-
-from messageFactory import generateNackMessage
-
-
-def send_nack(mqttc: mqtt.Client, message_id: str):
-    nack = generateNackMessage(message_id)
-
-    mqttc.publish("soarca", payload=nack.model_dump_json(), qos=1)
