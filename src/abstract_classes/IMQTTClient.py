2bf3d8327c83f698b773884d32c82555a44d193c
Maarten de Kruijf <maarten.dekruijf@tno.nl>
2024-03-27 11:02:15 +0000

    Feature/add abstract classes from source

---
+++ src/abstract_classes/IMQTTClient.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from paho.mqtt.client import Client, MQTTMessage, ConnectFlags
+from paho.mqtt.reasoncodes import ReasonCode
+from paho.mqtt.properties import Properties
+
+
+class IMQTTClient(ABC):
+
+    @abstractmethod
+    def on_connect(self, client: Client, userdata, connect_flags: ConnectFlags, reason_code: ReasonCode, properties: Properties):
+        pass
+
+    @abstractmethod
+    def on_message(self, client: Client, userdata, message: MQTTMessage):
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
