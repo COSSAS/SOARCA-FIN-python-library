a81d2e831f7380b351d7d3a7d3fd3a9fc25487e9
kroskinskiis <ivo.kroskinski@tno.nl>
2024-04-03 15:25:36 +0200

    Fix lint errors test_ack

--- tests/models/test_ack_message.py
+++ tests/models/test_ack_message.py
@@ -7,33 +7,35 @@ from soarca_fin_python_library.models.ack import Ack
 class TestAckMessage(unittest.TestCase):
 
     def test_ack_message_generator(self):
-        id = str(uuid1())
+        message_id = str(uuid1())
         ack = Ack(message_id=id)
         self.assertEqual(ack.type, "ack", "Type should be an ack")
-        self.assertEqual(ack.message_id, id, "Message_id's should match")
+        self.assertEqual(ack.message_id, message_id,
+                         "Message_id's should match")
 
     def test_json_to_ack(self):
 
-        id = str(uuid1())
+        message_id = str(uuid1())
 
         json_obj = {
             "type": "ack",
-            "message_id": id
+            "message_id": message_id
         }
 
         ack = Ack(**json_obj)
 
         self.assertEqual(ack.type, "ack", "Type should be an ack")
-        self.assertEqual(ack.message_id, id, "Message_id's should match")
+        self.assertEqual(ack.message_id, message_id,
+                         "Message_id's should match")
 
     def test_ack_to_json(self):
-        id = str(uuid1())
+        message_id = str(uuid1())
         ack = Ack(message_id=id)
 
         json_str = ack.model_dump_json()
 
         json_obj = {
-            "message_id": id,
+            "message_id": message_id,
             "type": "ack",
         }
 
