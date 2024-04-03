a98c12ed0f01fc21f51bf88c423cc319a1d74358
kroskinskiis <ivo.kroskinski@tno.nl>
2024-04-03 15:12:48 +0200

    Fix lint errors test_meta

--- tests/models/test_meta_message.py
+++ tests/models/test_meta_message.py
@@ -10,37 +10,37 @@ from soarca_fin_python_library.models.meta import Meta
 class testMetaMessage(unittest.TestCase):
 
     def test_meta_message_generator(self):
-        id = str(uuid1())
+        message_id = str(uuid1())
         timestamp = datetime.now(timezone.utc).isoformat()
-        metaMessage = Meta(sender_id=id, timestamp=timestamp)
+        metaMessage = Meta(sender_id=message_id, timestamp=timestamp)
 
-        self.assertEqual(metaMessage.sender_id, id)
+        self.assertEqual(metaMessage.sender_id, message_id)
         self.assertIsNotNone(metaMessage.timestamp)
         self.assertEqual(metaMessage.timestamp, timestamp)
 
     def test_json_to_meta_message(self):
-        id = str(uuid1())
+        message_id = str(uuid1())
         timestamp = datetime.now(timezone.utc).isoformat()
 
         json_object = {
-            "sender_id": id,
+            "sender_id": message_id,
             "timestamp": timestamp,
         }
 
         metaMessage = Meta(**json_object)
 
-        self.assertEqual(metaMessage.sender_id, id)
+        self.assertEqual(metaMessage.sender_id, message_id)
         self.assertEqual(metaMessage.timestamp, timestamp)
 
     def test_meta_message_to_json(self):
-        id = str(uuid1())
+        message_id = str(uuid1())
         timestamp = datetime.now(timezone.utc).isoformat()
-        metaMessage = Meta(sender_id=id, timestamp=timestamp)
+        metaMessage = Meta(sender_id=message_id, timestamp=timestamp)
 
         json_str = metaMessage.model_dump_json()
 
         json_object = {
-            "sender_id": id,
+            "sender_id": message_id,
             "timestamp": timestamp,
         }
 
